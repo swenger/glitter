43119c62896022829f6c7828391f30ceaba9da07
swenger <spwenger@googlemail.com>
2012-02-17 16:24:59 +0100

    add VertexArray binding

--- glitter/VertexArray.py
+++ glitter/VertexArray.py
@@ -8,7 +8,7 @@ class VertexArray(GLObject):
     _generate_id = _gl.glGenVertexArrays
     _delete_id = _gl.glDeleteVertexArrays
     _bind = _gl.glBindVertexArray
-    # TODO no binding
+    _bind = _gl.GL_VERTEX_ARRAY_BINDING
 
     # TODO
 
