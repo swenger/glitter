8d4ba86d2e473015c70f2660885e1a3406629d6e
swenger <spwenger@googlemail.com>
2012-03-01 13:55:50 +0100

    unify solid and border voxelization

--- examples/voxelization.py
+++ examples/voxelization.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
 
-"""
-import logging
-logger = logging.getLogger("rawgl")
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s: %(message)s")
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-"""
-
 import h5py
 
 from glitter import Framebuffer, ShaderProgram, TextureArray2D, uint32, VertexArray, Reset, get_default_context
@@ -34,45 +23,34 @@ fragment_code = """
 
 in vec4 ex_position;
 uniform float znear, zfar;
+uniform bool solid;
 layout(location=0) out uvec4 fragmentColor; // TODO multiple targets
 
-uvec4 border_voxelize(uint z, uint offset) {
+uvec4 voxelize(uint z, uint offset, uint fill, uint base) {
     if (z >= (offset + 1u) * 128u) return uvec4(0u);
-    if (z < offset * 128u) return uvec4(0u);
+    if (z < offset * 128u) return uvec4(fill);
     z -= offset * 128u;
     return uvec4(
-        z < 128u && z >= 96u ? 1u << (z - 96u) : 0u,
-        z <  96u && z >= 64u ? 1u << (z - 64u) : 0u,
-        z <  64u && z >= 32u ? 1u << (z - 32u) : 0u,
-        z <  32u             ? 1u <<  z        : 0u
+        z < 128u ? (z >= 96u ? base << (z - 96u) : fill) : 0u,
+        z <  96u ? (z >= 64u ? base << (z - 64u) : fill) : 0u,
+        z <  64u ? (z >= 32u ? base << (z - 32u) : fill) : 0u,
+        z <  32u ?             base << (z -  0u)         : 0u
     );
 }
 
-uvec4 solid_voxelize(uint z, uint offset) {
-    // switch on all bits >= z
-    if (z >= (offset + 1u) * 128u) return uvec4(0u);
-    if (z < offset * 128u) return uvec4(0xffffffffu);
-    z -= offset * 128u;
-    return uvec4(
-        z < 128u ? 0xffffffffu << (z >= 96u ? z - 96u : 0u) : 0u,
-        z <  96u ? 0xffffffffu << (z >= 64u ? z - 64u : 0u) : 0u,
-        z <  64u ? 0xffffffffu << (z >= 32u ? z - 32u : 0u) : 0u,
-        z <  32u ? 0xffffffffu <<             z             : 0u
-    );
-}
-
-void main() {
+void main() { // if solid, switch on all bits >= z; else, switch on bit == z
     uint z = uint(round(128.0 * (ex_position.z - znear) / (zfar - znear)));
-    fragmentColor = %s_voxelize(z, 0); // TODO multiple targets
+    fragmentColor = voxelize(z, 0, solid ? 0xffffffffu : 0u, solid ? 0xffffffffu : 1u); // TODO multiple targets
 }
 """
 
 def voxelize(filename, size, solid=True):
     num_targets = size // 128
 
-    shader = ShaderProgram(vertex=vertex_code, fragment=fragment_code % (num_targets, "solid" if solid else "border"))
+    shader = ShaderProgram(vertex=vertex_code, fragment=fragment_code % num_targets)
     shader.znear = -1
     shader.zfar = 1
+    shader.solid = solid
 
     volume = TextureArray2D(shape=(num_targets, size, size, 4), dtype=uint32)
     fbo = Framebuffer([volume[i] for i in range(len(volume))])
