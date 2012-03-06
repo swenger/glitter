4368ea7128fd850d3ee2b67243db47d92e75836f
swenger <spwenger@googlemail.com>
2012-03-06 17:09:24 +0100

    fix test_context to use get_current_context

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,7 +1,7 @@
 import numpy
 
 from glitter import EnumConstant
-from glitter.contexts import Context
+from glitter.contexts import get_current_context
 
 def check_property(context, name):
     value = getattr(context, name)
@@ -25,7 +25,7 @@ def check_property(context, name):
         pass # "AttributeError: can't set attribute" is okay for read-only attributes
 
 def test_property_generator():
-    context = Context()
+    context = get_current_context()
     properties = [x for x in dir(context) if not x.startswith("_")]
 
     for p in properties:
