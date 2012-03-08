808f2e70ad87a50abf88034182067a27c110f580
swenger <spwenger@googlemail.com>
2012-03-08 13:29:50 +0100

    fix with context in shader

--- glitter/shaders/shader.py
+++ glitter/shaders/shader.py
@@ -13,9 +13,9 @@ class Shader(ManagedObject):
     _delete_id = _gl.glDeleteShader
     _db = "shaders"
 
-    @classmethod
-    def release_compiler(cls):
-        _gl.glReleaseShaderCompiler()
+    def release_compiler(self):
+        with self._context:
+            _gl.glReleaseShaderCompiler()
 
     def __init__(self, source=None, compile=None):
         if any(x is NotImplemented for x in (self._type,)):
@@ -29,34 +29,38 @@ class Shader(ManagedObject):
             self.compile()
 
     def compile(self):
-        _gl.glCompileShader(self._id)
+        with self._context:
+            _gl.glCompileShader(self._id)
         
-        _compile_status = _gl.GLint()
-        _gl.glGetShaderiv(self._id, _gl.GL_COMPILE_STATUS, _gl.pointer(_compile_status))
-        if _compile_status.value != _gl.GL_TRUE:
-            raise ShaderCompileError(self._log)
+            _compile_status = _gl.GLint()
+            _gl.glGetShaderiv(self._id, _gl.GL_COMPILE_STATUS, _gl.pointer(_compile_status))
+            if _compile_status.value != _gl.GL_TRUE:
+                raise ShaderCompileError(self._log)
 
-        return self._log or None
+            return self._log or None
 
     @property
     def _log(self):
-        _info_log_length = _gl.GLint()
-        _gl.glGetShaderiv(self._id, _gl.GL_INFO_LOG_LENGTH, _gl.pointer(_info_log_length))
-        _info_log = _gl.create_string_buffer(_info_log_length.value)
-        _gl.glGetShaderInfoLog(self._id, _info_log_length, _gl.POINTER(_gl.GLint)(), _info_log)
-        return _info_log.value
+        with self._context:
+            _info_log_length = _gl.GLint()
+            _gl.glGetShaderiv(self._id, _gl.GL_INFO_LOG_LENGTH, _gl.pointer(_info_log_length))
+            _info_log = _gl.create_string_buffer(_info_log_length.value)
+            _gl.glGetShaderInfoLog(self._id, _info_log_length, _gl.POINTER(_gl.GLint)(), _info_log)
+            return _info_log.value
 
     @property
     def source(self):
-        _shader_source_length = _gl.GLint()
-        _gl.glGetShaderiv(self._id, _gl.GL_SHADER_SOURCE_LENGTH, _gl.pointer(_shader_source_length))
-        _source = _gl.create_string_buffer(_shader_source_length.value)
-        _gl.glGetShaderSource(self._id, _shader_source_length, _gl.POINTER(_gl.GLint)(), _source)
-        return _source.value
+        with self._context:
+            _shader_source_length = _gl.GLint()
+            _gl.glGetShaderiv(self._id, _gl.GL_SHADER_SOURCE_LENGTH, _gl.pointer(_shader_source_length))
+            _source = _gl.create_string_buffer(_shader_source_length.value)
+            _gl.glGetShaderSource(self._id, _shader_source_length, _gl.POINTER(_gl.GLint)(), _source)
+            return _source.value
 
     @source.setter
     def source(self, source):
-        _gl.glShaderSource(self._id, 1, _gl.pointer(_gl.c_char_p(source)), _gl.POINTER(_gl.GLint)())
+        with self._context:
+            _gl.glShaderSource(self._id, 1, _gl.pointer(_gl.c_char_p(source)), _gl.POINTER(_gl.GLint)())
 
 class VertexShader(Shader):
     """Vertes shader.
