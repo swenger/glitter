56ec5ef91c5e54a26be8452f68af0a055119f293
swenger <spwenger@googlemail.com>
2012-03-13 10:31:30 +0100

    increase version

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ class build_py(_build_py):
 
 setup(
     name = "glitter",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Stephan Wenger",
     author_email = "wenger@cg.cs.tu-bs.de",
     description = "Intuitive OpenGL wrappers",
