2471c9cb0de2298ab021787e5482df841a5a1f6b
swenger <spwenger@googlemail.com>
2012-05-22 11:03:50 +0200

    add key mappings to image viewer, fix support for gray images

--- examples/imageviewer.py
+++ examples/imageviewer.py
@@ -11,6 +11,7 @@ from numpy import array
 from PIL import Image
 
 from glitter import Texture2D, get_copy_pipeline_2d
+import glitter.raw as _gl
 from glitter.contexts.glut import GlutWindow, main_loop
 
 class ImageCache(object):
@@ -41,29 +42,74 @@ class ImageCache(object):
         return array(self.filenames[idx]) # TODO caching and preloading
 
 class IntroductionExample(object):
+    fps_scale = 1.1
+
     def __init__(self, images):
         self.window = GlutWindow(double=True, multisample=True)
         self.window.display_callback = self.display
         self.window.keyboard_callback = self.keyboard
+        self.window.special_callback = self.special
         self.images = images
         self.window.shape = self.images[0].shape[:2]
         self.render_pipeline = get_copy_pipeline_2d(context=self.window, sampler_type="isampler2D",
-                maxval=255, yscale=-1, use_framebuffer=False, image=Texture2D(self.images[0]))
+                maxval=255, yscale=-1, use_framebuffer=False, image=Texture2D(self._prepare_image(0)))
         self.current_index = 0
+        self.fps = 25
+        self.playing = False
+
+    def _prepare_image(self, idx):
+        img = self.images[idx]
+        while img.ndim < 3:
+            img = img[..., None]
+        return img
 
     def display(self):
         self.window.clear()
-        self.render_pipeline.image.data = self.images[self.current_index]
+        self.render_pipeline.image.data = self._prepare_image(self.current_index)
+        self.window.window_title = self.images.filenames[self.current_index].filename
         self.render_pipeline.draw()
         self.window.swap_buffers()
 
     def keyboard(self, key, x, y):
-        raise SystemExit # TODO play, pause, speed control, single frame
+        key = chr(key)
+        if key == "+":
+            self.fps *= self.fps_scale
+        elif key == "-":
+            self.fps /= self.fps_scale
+        elif key == " ":
+            if self.playing:
+                self.playing = False
+            else:
+                self.playing = True
+                self.timer()
+        elif key == chr(27):
+            raise SystemExit
+
+    def special(self, key, x, y):
+        if key == _gl.GLUT_KEY_LEFT:
+            self.current_index = (self.current_index - 1) % len(self.images)
+            self.window.post_redisplay()
+        elif key == _gl.GLUT_KEY_RIGHT:
+            self.current_index = (self.current_index + 1) % len(self.images)
+            self.window.post_redisplay()
+        if key == _gl.GLUT_KEY_PAGE_UP:
+            self.current_index = (self.current_index - self.fps) % len(self.images)
+            self.window.post_redisplay()
+        elif key == _gl.GLUT_KEY_PAGE_DOWN:
+            self.current_index = (self.current_index + self.fps) % len(self.images)
+            self.window.post_redisplay()
+        elif key == _gl.GLUT_KEY_HOME:
+            self.current_index = 0
+            self.window.post_redisplay()
+        elif key == _gl.GLUT_KEY_END:
+            self.current_index = len(self.images) - 1
+            self.window.post_redisplay()
 
     def timer(self):
-        self.current_index = (self.current_index + 1) % len(self.images)
-        self.window.add_timer(1000 / 25, self.timer)
-        self.window.post_redisplay()
+        if self.playing:
+            self.current_index = (self.current_index + 1) % len(self.images)
+            self.window.add_timer(int(round(1000.0 / self.fps)), self.timer)
+            self.window.post_redisplay()
 
     def run(self):
         self.timer()
