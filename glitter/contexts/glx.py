bb6af3b0a9317ccc81bb8742f398a72a1f989f9f
Stephan Wenger <wenger@cg.cs.tu-bs.de>
2012-09-21 05:26:17 -0600

    GLX context now binds itself after creation

--- glitter/contexts/glx.py
+++ glitter/contexts/glx.py
@@ -12,6 +12,7 @@ try:
     from glxcontext import GLXContext as _GLXContext
 
     from glitter.contexts.context import Context
+    from glitter.contexts.contextmanager import context_manager
 
     class GLXContext(_GLXContext, Context):
         """Offscreen GLX context."""
@@ -19,6 +20,14 @@ try:
         def __init__(self, **kwargs):
             _GLXContext.__init__(self, **kwargs)
             Context.__init__(self)
+
+            # TODO the lines below should not be necessary, or should at least be performed automatically by context_manager
+            # XXX I would have expected it worked without these lines because of "with self._context" around GLObject functions; is this a problem with multiple Contexts?
+            old_binding = context_manager.current_context
+            if old_binding:
+                old_binding._bind()
+            else:
+                context_manager.current_context = self
 
         def _bind(self):
             return _GLXContext.bind(self)
