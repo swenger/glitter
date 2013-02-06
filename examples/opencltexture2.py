b9f968ea2360f344445d1d5f7c025d9d63c2dec3
Kai Ruhl <ruhl@cg.tu-bs.de>
2013-02-06 17:24:55 +0100

    Fixed on GTX590 by taking second device.

--- examples/opencltexture2.py
+++ examples/opencltexture2.py
@@ -12,6 +12,13 @@ import pyopencl as cl
 from glitter import Texture2D
 from glitter.raw import gl
 
+cl_source = """
+const sampler_t T_RAW_SAMPLER = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
+
+__kernel void run(uint wid, uint hei, __read_only image2d_t img0) {
+}
+"""
+
 
 def get_gl_context(option="g"):
     """Returns an OpenGL context. Options: g(lut), q(t)"""
@@ -38,19 +45,31 @@ def get_cl_context(gl_context):
             from pyopencl.tools import get_gl_sharing_context_properties
             cl_platform = cl.get_platforms()[0]
             cl_properties = [(cl.context_properties.PLATFORM, cl_platform)] + get_gl_sharing_context_properties()
-            cl_devices = [cl_platform.get_devices()[0]]  # Only one is allowed!
+            cl_devices = [cl_platform.get_devices()[-1]]  # Only one is allowed!
             cl_context = cl.Context(properties=cl_properties, devices=cl_devices)
     else: # ... or in stand-alone mode, CL context without GL?
         cl_platform = cl.get_platforms()[0]  # @UndefinedVariable
         cl_properties = [(cl.context_properties.PLATFORM, cl_platform)]
-        cl_devices = [cl_platform.get_devices()[0]]  # Only one is allowed!
+        cl_devices = [cl_platform.get_devices()[-1]]  # Only one is allowed!
         cl_context = cl.Context(properties=cl_properties, devices=cl_devices)
     return cl_context
 
 
-#if __name__ == "__main__":
-gl_context = get_gl_context("q" if len(sys.argv) < 2 else sys.argv[1]) # fails with g, works with q
-cl_context = get_cl_context(gl_context)
-from scipy.misc import lena; img = np.dstack([lena() / 256.] * 3).astype(np.float32)
-gl_img = Texture2D(img, mipmap=False, context=gl_context)
-cl_img = cl.GLTexture(cl_context, cl.mem_flags.READ_ONLY, gl.GL_TEXTURE_2D, 0, gl_img._id, 2)
+if __name__ == "__main__":
+    gl_context = get_gl_context("g" if len(sys.argv) < 2 else sys.argv[1])
+    cl_context = get_cl_context(gl_context)
+    from scipy.misc import lena;
+    img = np.dstack([lena() / 256.] * 3).astype(np.float32); hei, wid = img.shape[:2]
+    gl_img = Texture2D(img, mipmap=True, context=gl_context)
+    cl_img = cl.GLTexture(cl_context, cl.mem_flags.READ_ONLY, gl.GL_TEXTURE_2D, 1, gl_img._id, 2)
+    cl_queue = cl.CommandQueue(cl_context)
+    cl_program = cl.Program(cl_context, cl_source).build()
+    if True: # usable in loop
+        cl_gl_data = [cl_img]
+        cl.enqueue_acquire_gl_objects(cl_queue, cl_gl_data)
+        cl_args = [np.uint32(wid), np.uint32(hei), cl_img]; assert 3 == len(cl_args)
+        cl_program.run(cl_queue, (wid, hei), None, *cl_args)
+        cl.enqueue_release_gl_objects(cl_queue, cl_gl_data)
+        cl_queue.flush()
+    cl_queue.finish()
+    print "Finished."
